48ff94d6f7a0554f46b2a5a19c82285e6c3239d2
Md Arman <armanar6712@gmail.com>
2025-02-16 16:31:33 +0530

    add useFetch composable

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,10 @@
-<script setup>
-import { onMounted } from 'vue';
-import useAuth from '@/composables/useAuth';
-
-const { storeAuthToken } = useAuth();
-
-onMounted(() => {
-  const token = new URLSearchParams(window.location.hash.replace('#', '?')).get('access_token');
-  if (token) {
-    storeAuthToken();
-    window.location.href = '/';
-  }
-});
-</script>
+<script setup></script>
 
 <template>
   <main class="text-center py-8 px-10">
     <h1 class="text-3xl uppercase font-bold mb-4">Spotify Web App Clone</h1>
-    <p class="text-lg">Using VueJs, Vue Router, Pinia Store, Spotify Web Api, Tailwind Css, Axios</p>
+    <p class="text-lg">
+      Using VueJs, Vue Router, Pinia Store, Spotify Web Api, Tailwind Css, Axios
+    </p>
   </main>
 </template>
